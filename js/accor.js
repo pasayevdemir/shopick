0d2f6800719f8622f60e142559619fe79981d017
Inji Zakariyayeva <incizekeriyeyeva@gmail.com>
2023-09-23 16:16:27 +0400

    checkout and responsive completed

---
+++ js/accor.js
@@ -0,0 +1,20 @@
+function accordionFunc() {
+    const opener = document.querySelectorAll('.accordion-opener');
+
+    opener.forEach(item=>{
+      item.addEventListener('click', (e)=>{
+        const accord = item.parentElement.querySelector('.checkout-login');
+
+        opener.forEach(x=>{
+            x.parentElement.querySelector('.checkout-login').classList.remove('accor-opened');
+            x.getElementsByTagName('input')[0].checked = false;
+        })
+
+
+        accord.classList.toggle('accor-opened');
+        item.getElementsByTagName('input')[0].checked = true;
+
+      })
+    })
+  }
+  accordionFunc();
